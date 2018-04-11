50c7f7b01b792d33feb5e0c1d49234eb7e1da6c7
Unknown <tara.yankee@uconn.edu>
2018-04-11 11:46:57 -0400

    DESeq2 Tutorial for Recount2 data

---
+++ DESeq2_Analysis_Recount2_Output.R
@@ -0,0 +1,9 @@
+##This file gives an example with explanations for how to perform #some DESeq2 analysis from Recount2 count tables. 
+
+##Libraries Required 
+
+library(sva)
+library(recount)
+library(DESeq2)
+
+
