c34b1456987555b0ffa28d510066f8f6e8d14399
Unknown <tara.yankee@uconn.edu>
2018-04-17 15:33:00 -0400

    Added to RDAVID

--- RDavidWebService.R
+++ RDavidWebService.R
@@ -1,5 +1,6 @@
 ##Tutorial for using RDAVIDWebService 
 
+##Helpful Site https://gist.github.com/svigneau/9699239
 
 ##You first have to register at https://david.ncifcrf.gov/webservice/register.htm
 
@@ -8,5 +9,14 @@
 
 library(rJava)
 library(RDAVIDWebService)
+
+##Create david object 
+
+david<- DAVIDWebService(email="yankee@uchc.edu",
+	url = "https://david.ncifcrf.gov/webservice/services/		 DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
+
+
+
+
 
 
