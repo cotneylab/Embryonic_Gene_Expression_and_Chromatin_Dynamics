af2a32660f09c31fef6fc6e332fa501527ddd3ce
Unknown <tara.yankee@uconn.edu>
2019-04-14 19:39:34 -0400

    Added short code for pie tss pie charts

---
+++ TSS_distance_piechart.R
@@ -0,0 +1,15 @@
+cout<-0
+cin<-0
+for (i in dss_filt){
+  
+  if (i >= 2000){
+    cout<-cout+1
+  }
+  else if (i <2000){
+    cin<-cin+1
+  }
+  
+}
+slices<-c(cout,cin)
+lbls<-c("TSS distal","TSS proximal")
+pie(slices,labels=lbls)
