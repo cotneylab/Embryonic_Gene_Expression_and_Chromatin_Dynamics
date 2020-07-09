ca3ade65a66d652150edd13c31c56c3647f5ded3
Unknown <yankee@uchc.edu>
2020-07-09 18:08:51 -0400

    SVA code from Dr. Cotney
    SVA code to get SVA transformation of DESEq2 counts.

---
+++ Heart_Project/sva_from_DESeq2.R
@@ -0,0 +1,28 @@
+##General code to get SV corrected counts from DESeq2 objects
+
+library(DESeq2)
+
+##load your ddssva and SVs
+load("<ddssva filename>")
+load("<svseq filename>")
+
+##Extract the counts
+dat<-counts(ddssva,normalized=TRUE)
+
+## turn counts dataframe into type matrix
+dat<-as.matrix(dat)
+
+##transpose of count matrix
+Y<- t(dat)
+
+## Extract just the SVs
+W<- svseq$sv
+
+##complicated matrix math
+alpha <- solve(t(W) %*% W) %*% t(W) %*% Y
+
+##Add sva batch corrected count values to the svseq object
+svseq$corrected<- t(Y- W %*% alpha)
+
+##save your svseq object
+save(svseq,file="HumanHeart_rnaseq_SVcorrect.Rdata")
